fd7231edc4d6e26b9e7473a86370a07ae9016e34
GamGas <gasanov.gamid.1993@gmail.com>
2020-07-22 23:22:28 +0300

    Тестирование подключения GIT к IntelliJ IDEA

--- src/goldman/controller/MotionHandler.java
+++ src/goldman/controller/MotionHandler.java
@@ -8,6 +8,7 @@ public class MotionHandler {
 
 
     //Метод, меняющий объекты в координатах местами. Для перемещения.
+    //Впрочем, обычный метод с временной переменной
     public static void swap(Coordinate prev, Coordinate next){
         Item tempItem = prev.getItem();
         prev.setItem(next.getItem());
